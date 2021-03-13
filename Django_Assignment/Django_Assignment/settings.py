209c2a935d0ddd707d5668ebe429bf0c937638e9
Samir <Saitwalsamir@gmail.com>
2021-03-13 11:59:14 +0530

    --blogsite model added

--- Django_Assignment/Django_Assignment/settings.py
+++ Django_Assignment/Django_Assignment/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'rest_auth.registration',
     'drf_yasg',
+    'blogsite',
 ]
 
 MIDDLEWARE = [
--- Django_Assignment/blogsite/models.py
+++ Django_Assignment/blogsite/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    blog = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return str(self.title)
